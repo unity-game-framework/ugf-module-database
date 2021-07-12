1cbaed85a55956198456d4b755414967dffa9ace
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-07-12 20:38:29 +0300

    Add MemoryDatabaseAsset and PrefsDatabaseAsset

---
+++ Packages/UGF.Module.Database/Runtime/Memory/MemoryDatabaseAsset.cs
@@ -0,0 +1,15 @@
+﻿using UGF.Database.Runtime;
+using UGF.Database.Runtime.Memory;
+using UnityEngine;
+
+namespace UGF.Module.Database.Runtime.Memory
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Database/Database Memory", order = 2000)]
+    public class MemoryDatabaseAsset : DatabaseBuilderAsset
+    {
+        protected override IDatabase OnBuild()
+        {
+            return new MemoryDatabase();
+        }
+    }
+}
